c9601784d92a809f7010a3a9bc1129035065d286
OliwiaWawrzynek <oliwiawawrzynek@gmail.com>
2023-01-07 16:47:27 +0100

    update

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,7 @@ const input = document.querySelector("input")
 const creation = document.querySelector("[data-create");
 const destruction = document.querySelector("[data-destroy]");
 const amount = document.querySelector("input")
+let sizeOfBox = 20;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -10,20 +11,28 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-creation.addEventListener("click", (event) =>
-    createBoxes(amount.value));
-
+creation.addEventListener("click", (event) => {
+    
+  createBoxes(amount.value)
+});
+   
 function createBoxes(amount) {
+  
+  for (let i = boxes.children.length - 1; i >= 0; i--) {
+        boxes.children[i].remove();
+  };
+
   for (let i = 1; i <= amount; i++) {
     const newBox = document.createElement("div");
     boxes.append(newBox);
-    let sizer = 20 + i * 10;
+    const sizer = sizeOfBox + i * 10;
+    newBox.className = "box";
     newBox.style.width = `${sizer}px`;
     newBox.style.height = `${sizer}px`;
     newBox.style.backgroundColor = getRandomHexColor();
   }
 };
-
+  
 destruction.addEventListener("click", function destroyBoxes(event) {
     for (let i = boxes.children.length - 1; i >= 0; i--) {
         boxes.children[i].remove();
